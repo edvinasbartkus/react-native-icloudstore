492e4f4d665f4a4cf443fd3d82f353e1cc30f78c
Edvinas Bartkus <edvinas@bartkus.lt>
2019-09-30 19:03:28 +0300

    Run in main queue

--- iCloudStorage.m
+++ iCloudStorage.m
@@ -17,6 +17,11 @@ static NSString* const kChangedKeys = @"changedKeys";
 
 @implementation iCloudStorage
 
++ (BOOL)requiresMainQueueSetup
+{
+  return YES;
+}
+
 + (NSString*)appendPrefixToKey:(NSString*)key {
   return [NSString stringWithFormat:@"%@%@", ICLOUDSTORAGE_PREFIX, key];
 }
